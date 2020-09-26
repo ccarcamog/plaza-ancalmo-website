79181bcdec325648e6bf542df7b64f9ed3527d31
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2020-09-26 12:16:43 -0600

    Stylized navbar

--- components/navbar.php
+++ components/navbar.php
@@ -1,24 +1,36 @@
-<nav class="navbar navbar-expand-md navbar-light bg-light" id="top-navbar">
-  <a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light" id="top-navbar">
+
+  <!-- Brand -->
+  <a class="navbar-brand" href="#">
+    <img src="img/logo.png" width=80px alt="Logo de Plaza Ancalmo">
+  </a>
+  
+  <a href="/" class="navbar-brand">PLAZA <br>ANCALMO</a>
+  
+  <!-- toggle button -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
+  <!-- Items de la navbar -->
   <div class="collapse navbar-collapse" id="navbarNav">
     <div class="navbar-nav">
+      <a class="nav-link" href="#">Farmacia</a>
       <a class="nav-link" href="#">Laboratorio Menendez</a>
       <a class="nav-link" href="#">Lifestyle</a>
-      <div class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+      <div class="dropdown">
+        <a class="nav-link dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Especialidades
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <p class="dropdown-header">Populares</p>
           <a class="dropdown-item" href="#">Odontologia</a>
           <a class="dropdown-item" href="#">Cardiologia</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Cirugia</a>
+          
+          <p class="dropdown-header">Todos</p>
         </div>
       </div>
+
       <a class="nav-link" href="#">Acerca de</a>
       <a class="nav-link" href="#">Locales disponibles</a>
     </div>
