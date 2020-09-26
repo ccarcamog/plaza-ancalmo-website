8621cc2d7d348e0e98400977764a27971cc526a2
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2020-09-26 00:05:37 -0600

    Primer push a git, descargado bootstrap y empezado la navbar

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Plaza ANCALMO</title>
+
+	<!-- MDB icon -->
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Material Design Bootstrap -->
+	<!-- <link rel="stylesheet" href="css/mdb.min.css"> -->
+	<!-- Your custom styles (optional) -->
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+	<?php include 'components/navbar.php' ?>
+
+
+	<!-- jQuery -->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<!-- Bootstrap core JavaScript -->
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<!-- MDB core JavaScript -->
+	<!-- <script type="text/javascript" src="js/mdb.min.js"></script> -->
+	<!-- Your custom scripts (optional) -->
+
+</body>
+
+</html>
