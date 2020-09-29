20d461c21eaf1ff64b858a0aaf713a1179f9db90
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2020-09-29 14:08:18 -0600

    Iniciada la pagina de farmacia

--- farmacia/index.php
+++ farmacia/index.php
@@ -22,6 +22,150 @@
 
 	<?php include "../components/navbar.php" ?>
 
+	<header class="container-fluid" id="farmaciaHeader">
+		<div>
+
+			<h1 class="text-white">
+				<img class="svg-inverted" src="/img/svg/noun_Pharmacy_3125931.svg" alt="pharmacy image" height="150">
+				FARMACIA <br>ANCALMO
+			</h1>
+		</div>
+	</header>
+
+	<div class="container text-center mt-5 p-3" id="introFarmacia">
+		<h2>Tenemos lo que necesitas</h2>
+		<hr>
+		<p>Nuestra visión de servicio nos inspira a trabajar con excelencia, ofreciéndote medicamentos y productos de calidad. Somos tu lugar de confianza.</p>
+		<p>
+			<a href="https://www.facebook.com/FarmaciasAncalmo/">
+				<img src="/img/svg/facebook.svg" alt="facebook logo" />
+			</a>
+			<a href="https://www.instagram.com/FarmaciaAncalmo/">
+				<img src="/img/svg/instagram.svg" alt="facebook logo">
+			</a>
+		</p>
+	</div>
+
+	<hr>
+
+	<div class="container" id="domicilioInfo">
+		<center>
+			<h2>Servicio a domicilio</h2>
+		</center>
+		<div class="row p-3">
+			<div class="col-md-6 text-center p-3">
+				<h3> <i> TELEFONOS: </i> </h3>
+				<p>2243-1000 <br> 7860-2663 </p>
+				<p> <img src="/img/svg/whatsapp.svg" width=40 alt="whatsapp logo"></p>
+			</div>
+			<div class="col-md-6 p-3" id="domicilioLugares">
+				<div>
+					<h3><i>ENVIOS A</i></h3>
+
+					<Ul>
+						<li>ANTIGUO CUSCATLAN</li>
+						<li>COLONIA ESCALON</li>
+						<li>CIUDAD MERLIOT</li>
+						<li>SANTA TECLA</li>
+					</Ul>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="container" id="farmaciaPuntos">
+		<div class="row p-3">
+			<div class="col-md-4 text-white p-4 text-center">
+				<img class="svg-inverted" src="/img/svg/check.svg" height=100 alt="conveniencia icon">
+				<hr class="border-white">
+				<h3><b>Conveniencia</b></h3>
+				<p>Contamos con servicio a domicilio, pago de servicios y muchas cosas mas.</p>
+			</div>
+			<div class="col-md-4 p-4 text-white text-center">
+				<img src="/img/svg/security.svg" alt="seguridad icon" height=100 class="svg-inverted">
+				<hr class="border-white">
+				<h3>Seguridad</h3>
+				<p>Seguimos protocolos de bioseguridad y contamos con seguridad para tu confianza.</p>
+
+			</div>
+			<div class="col-md-4 p-4 text-white text-center">
+				<img src="/img/svg/variety.svg" alt="seguridad icon" height=100 class="svg-inverted">
+				<hr class="border-white">
+				<h3>Variedad</h3>
+				<p>Contamos con una amplia variedad de productos de la mas alta calidad.</p>
+			</div>
+		</div>
+	</div>
+
+
+	<hr>
+
+	<div class="container p-4">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="card">
+					<!-- Card image -->
+					<img class="card-img-top" src="https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/118474464_154252323017826_1934432058028117513_o.jpg?_nc_cat=101&_nc_sid=730e14&_nc_ohc=m8GKUHRM0ooAX-zCF6z&_nc_ht=scontent-mia3-1.xx&oh=e204ced8e18106f9fdfb1bbef52368c2&oe=5F9B1628" alt="Card image cap">
+
+					<!-- Card content -->
+					<div class="card-body">
+
+						<!-- Title -->
+						<h4 class="card-title"><a>Mascarilla quirúrgica</a></h4>
+						<!-- Text -->
+						<p class="card-text">50 unidades $36.50 <br>Individual $0.73</p>
+
+
+					</div>
+
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card">
+
+					<!-- Card image -->
+					<img class="card-img-top" src="https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/118342241_152213846555007_1220169973404806707_o.png?_nc_cat=109&_nc_sid=730e14&_nc_ohc=PDJ-v7epUnMAX8eBw54&_nc_ht=scontent-mia3-2.xx&oh=e142c7e6d98c645fb3558c6150e40ef4&oe=5F9928E4" alt="Card image cap">
+
+					<!-- Card content -->
+					<div class="card-body">
+
+						<!-- Title -->
+						<h4 class="card-title"><a>Mascarilla <br>
+								KN95</a></h4>
+						<!-- Text -->
+						<p class="card-text">10 unidades $22.50. <br>Individual $2.5</p>
+
+
+					</div>
+
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card">
+
+					<!-- Card image -->
+					<img class="card-img-top" src="https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/118183109_151071413335917_7242190392973451981_o.jpg?_nc_cat=108&_nc_sid=730e14&_nc_ohc=M2pXtklntgQAX_tSTJ8&_nc_ht=scontent-mia3-1.xx&oh=a082dc846bce59c3067833280ae42374&oe=5F9A82B3" alt="Card image cap">
+
+					<!-- Card content -->
+					<div class="card-body">
+
+						<!-- Title -->
+						<h4 class="card-title"><a>Alcohol gel ancalmo</a></h4>
+						<!-- Text -->
+						<p class="card-text"> galon $11.95 <br>la botella $</p>
+
+
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php include "../components/footer.php" ?>
+
 
 	<!-- Bootstrap core JavaScript -->
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
