b49c89c248f4af0786db6050cd5b3f6a67fdaefd
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2020-09-29 21:33:28 -0600

    Añadido placeholder de carousel

--- farmacia/index.php
+++ farmacia/index.php
@@ -25,7 +25,6 @@
 	<header class="container-fluid" id="farmaciaHeader">
 		<div>
 			<img src="/img/svg/Logo Farmacia Ancalmo copy.svg" alt="Logo Farmacia Ancalmo">
-
 		</div>
 	</header>
 
@@ -85,7 +84,7 @@
 	</div>
 
 	<div class="container-fluid" id="bancoServices">
-	
+
 		<div class="container">
 			<div class="row">
 				<div class="col-md-3">
@@ -148,19 +147,53 @@
 			</div>
 		</div>
 	</div>
-
-
-
-
-
+	<div class="container">
+		
+		<div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+		
+			<ol class="carousel-indicators">
+				<li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+				<li data-target="#carousel-example-1z" data-slide-to="1"></li>
+				<li data-target="#carousel-example-1z" data-slide-to="2"></li>
+			</ol>
+		
+			<div class="carousel-inner" role="listbox">
+				
+				<div class="carousel-item active">
+					<img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg" alt="First slide">
+				</div>
+				
+				<div class="carousel-item">
+					<img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg" alt="Second slide">
+				</div>
+				
+				<div class="carousel-item">
+					<img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" alt="Third slide">
+				</div>
+				
+			</div>
+			
+			<a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+			
+		</div>
+		
+	</div> 
 
 	<?php include "../components/footer.php" ?>
 
 
+	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<!-- Bootstrap core JavaScript -->
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<!-- MDB core JavaScript -->
-	<!-- <script type="text/javascript" src="js/mdb.min.js"></script> -->
+	<script type="text/javascript" src="js/mdb.min.js"></script>
 	<!-- Your custom scripts (optional) -->
 </body>
 
