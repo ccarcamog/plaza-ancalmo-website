bf68ce87872ce2bcad045752aa77a4feff402390
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2020-10-15 11:57:21 -0600

    Modificado banner

--- index.php
+++ index.php
@@ -36,12 +36,12 @@
 				</div>
 				<div id="item2" class="carousel-item">
 					<div class="filter">
-						<h1>A la vanguardia de tu salud</h1>
+						<h1>Tu lugar de confianza</h1>
 					</div>
 				</div>
 				<div id="item3" class="carousel-item">
 					<div class="filter">
-						<h1 class="text-align-center">A la vanguardia de tu salud</h1>
+						<h1 class="text-align-center">Sirviendo excelencia... Con amor</h1>
 					</div>
 				</div>
 			</div>
