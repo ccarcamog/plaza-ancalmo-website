31551432a2c7254bf17cf8c6479e157c560855c7
Luis Mario Ramirez Peralta <luismarioram99@gmail.com>
2020-11-03 18:30:43 -0600

    cambiado correo en credentials

--- php/credentials.php
+++ php/credentials.php
@@ -3,6 +3,6 @@ $dbHost = "92.249.44.207";
 $dbName	= "u526609065_plazaancalmo";
 $dbUID	= "u526609065_app";
 $dbPWD  = "0UL]Xe;d";
-$contactMailReceiver = "wasd@plazaancalmo.com"
+$contactMailReceiver = "luismarioram99@gmail.com"
 ?>
 
